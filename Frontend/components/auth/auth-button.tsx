0c4b73d287a75a3e13bad43c63893dfbce6153e1
Sathya-Ganesh007 <ganeshveleappa@gmail.com>
2026-01-31 15:20:09 +0530

    database

--- Frontend/components/auth/auth-button.tsx
+++ Frontend/components/auth/auth-button.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
-import { Button } from "./ui/button";
 import { createClient } from "@/lib/supabase/server";
-import { LogoutButton } from "./logout-button";
 
 export async function AuthButton() {
   const supabase = await createClient();
@@ -12,18 +10,8 @@ export async function AuthButton() {
   const user = data?.claims;
 
   return user ? (
-    <div className="flex items-center gap-4">
-      Hey, {user.email}!
-      <LogoutButton />
-    </div>
+    <div className="flex items-center gap-4">Hey, {user.email}!</div>
   ) : (
-    <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/auth/login">Sign in</Link>
-      </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/auth/sign-up">Sign up</Link>
-      </Button>
-    </div>
+    <div className="flex gap-2"></div>
   );
 }
